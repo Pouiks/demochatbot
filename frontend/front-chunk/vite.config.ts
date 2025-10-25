831534767acbb699b6888d0d11ac4836ad7d8202
Pouiks <virgilejoinville@gmail.com>
2025-10-25 16:40:37 +0200

    change vite config to autorize other ips

--- frontend/front-chunk/vite.config.ts
+++ frontend/front-chunk/vite.config.ts
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: false,
+    allowedHosts: ['*'] // Accepter tous les domaines
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: false
+  }
 })
